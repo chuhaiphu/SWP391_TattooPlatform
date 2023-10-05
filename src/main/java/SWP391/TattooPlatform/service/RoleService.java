afffd28d0e73ca503ea311c589118f54e1fb052a
QuyDuong162 <duonghqse171776@fpt.edu.vn>
2023-10-05 16:55:09 +0700

    Repo Update

--- src/main/java/SWP391/TattooPlatform/service/RoleService.java
+++ src/main/java/SWP391/TattooPlatform/service/RoleService.java
@@ -1,2 +1,50 @@
-package SWP391.TattooPlatform.Service;public class RoleService {
+package SWP391.TattooPlatform.service;
+
+import SWP391.TattooPlatform.model.Role;
+import SWP391.TattooPlatform.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class RoleService {
+    final  RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository){
+        this.roleRepository = roleRepository;
+    }
+
+
+
+    public List<Role> getListRole() {
+        if(roleRepository.findAll().isEmpty()) {
+            return null;
+        }
+        return roleRepository.findAll();
+    }
+
+    public Role addRole(Role role)
+    {
+        return roleRepository.save(role);
+    }
+
+    public Role updateRole(String roleID
+            , String name ) throws Exception {
+
+        roleRepository.updateRole(roleID, name);
+        return roleRepository.findRolesByRoleID(roleID);
+    }
+
+    public Role deleteRole(String id) throws Exception{
+        roleRepository.deleteRolesByID(id);
+        Role role = roleRepository.findRolesByRoleID(id);
+        if(role == null) {
+            return null;
+        }else {
+            throw new Exception();
+        }
+    }
 }
+
+
