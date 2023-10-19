fc72f1d9aa41f94fac5cff16446822d921c1366d
Phuc Loc <locdpse171702@fpt.edu.vn>
2023-10-19 15:52:51 +0700

    delete role at DucAnh

--- src/main/java/SWP391/TattooPlatform/repository/TattooLoversRepository.java
+++ src/main/java/SWP391/TattooPlatform/repository/TattooLoversRepository.java
@@ -1,12 +1,9 @@
 package SWP391.TattooPlatform.repository;
-import SWP391.TattooPlatform.model.Role;
 import SWP391.TattooPlatform.model.TattooLovers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
