8928f0890495fdb286039976746e602e777f9538
hadinhducanh <ducanhhadinh@gmail.com>
2023-10-25 00:42:26 +0700

    Add lỏ

--- src/main/resources/static/studiomanager/assets/js/update-artist.js
+++ src/main/resources/static/studiomanager/assets/js/update-artist.js
@@ -1,39 +1,44 @@
-$(document).ready(function () {
-    $("#update-artist").click(function () {
-        // Get the values from input fields
-        var email = $("#emailArtist").val();
-        var fullName = $("#updateNameArtist").val();
-        var phoneNumber = $("#updatePhoneArtist").val();
-        var address = $("#updateAddressArtist").val();
 
-        // Create a data object to send via AJAX
-        var data = {
-            email: email,
-            fullName: fullName,
-            phoneNumber: phoneNumber,
-            address: address
-        };
+class Artist {
+    constructor(email, fullName, phoneNumber, address,rate, username,password ) {
+        this.email= email;
+        this.fullname = fullname;
+        this.phoneNumber =phoneNumber;
+        this.address = address;
+        this.rate = rate;
+        this.username = username;
+        this.password = password;
+    }
+}
 
-        // Send an AJAX POST request to update the artist
-        $.ajax({
-            type: "PUT",
-            url: "/view-artist/update-artist/" + email, // Replace with the actual API endpoint
-            contentType: "application/json; charset-utf-8",
-            data: JSON.stringify(data),
-            success: function (data) {
-                // Handle the success response
-                console.log("Artist updated successfully:", data);
 
-                // Optionally, you can display a success message and redirect the user.
-                // For example, you can redirect back to the artist list page:
-                window.location.href = "view-artist.html";
-            },
-            error: function (xhr, status, error) {
-                // Handle the error
-                console.error("Error updating artist: " + error);
+document.getElementById("update-artist").addEventListener("click", function () {
+    // Enable input fields
+    let email = document.getElementById('email').value
+    let fullname = document.getElementById('fullName').value
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+    let phoneNumber = document.getElementById('phoneNumber').value
+    let address = document.getElementById('address').value
+    let rate = document.getElementById('rate').value
 
-                // Optionally, you can display an error message to the user.
-            }
-        });
-    });
+    let Artist = new Artist(email, fullname, phoneNumber, address, rate, username, password );
+
+    sessionStorage.setItem('Artist', JSON.stringify(Artist));
+    window.location.href = "/update-artist.html";
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/resources/static/studiomanager/assets/js/update-artistpro.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    const Artist = JSON.parse(sessionStorage.getItem('Artist'));
+    document.getElementById('emailArtist').value = Artist.content.email;
+    document.getElementById('updateNameArtist').value = Artist.content.fullname;
+    document.getElementById('updateAddressArtist').value = Artist.content.phonenumber;
+    document.getElementById('updatePhoneArtist').value = Artist.content.address;
+    $("#update-artist").click(function () {
+
+        // Get the values from input fields
+        var email = $("#emailArtist").val();
+        var fullName = $("#updateNameArtist").val();
+        var phoneNumber = $("#updatePhoneArtist").val();
+        var address = $("#updateAddressArtist").val();
+
+        // Create a data object to send via AJAX
+        var data = {
+            email: email,
+            fullName: fullName,
+            phoneNumber: phoneNumber,
+            address: address
+        };
+
+        // Send an AJAX POST request to update the artist
+        $.ajax({
+            type: "PUT",
+            url: "/view-artist/update-artist/" + email, // Replace with the actual API endpoint
+            contentType: "application/json; charset-utf-8",
+            data: JSON.stringify(data),
+            success: function (data) {
+                // Handle the success response
+                console.log("Artist updated successfully:", data);
+
+                // Optionally, you can display a success message and redirect the user.
+                // For example, you can redirect back to the artist list page:
+                window.location.href = "view-artist.html";
+            },
+            error: function (xhr, status, error) {
+                // Handle the error
+                console.error("Error updating artist: " + error);
+
+                // Optionally, you can display an error message to the user.
+            }
+        });
+    });
+});
+
