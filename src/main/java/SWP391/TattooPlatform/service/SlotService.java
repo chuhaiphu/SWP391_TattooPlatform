4bc9ab92f9fb368121c8f532bcb9c255ed99f9f3
Phuc Loc <locdpse171702@fpt.edu.vn>
2023-11-02 16:16:24 +0700

    fix get slot by date and studio id

--- src/main/java/SWP391/TattooPlatform/service/SlotService.java
+++ src/main/java/SWP391/TattooPlatform/service/SlotService.java
@@ -7,8 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,12 +28,30 @@ public class SlotService {
             return ResponseUtils.get(slotRepository.findSlotsByStudioID(id),HttpStatus.FOUND);
         }
 
-        public ResponseEntity<?> getListSlotByStudioIDAndDate(String id, String date) {
-            if(slotRepository.findSlotsByStudioIDAndDate(id,date).isEmpty()) {
-                return ResponseUtils.error("Not found any slot in this Studio", HttpStatus.FOUND);
-            }
-            return ResponseUtils.get(slotRepository.findSlotsByStudioIDAndDate(id,date),HttpStatus.OK);
+    public ResponseEntity<?> getListSlotByStudioIDAndDate(String id, String date) {
+        // Get the current real-time LocalDateTime
+
+        if (slotRepository.findSlotsByStudioIDAndDate(id, date).isEmpty()) {
+            return ResponseUtils.error("Not found any slot in this Studio", HttpStatus.FOUND);
         }
+
+        List<Slot> slots = slotRepository.findSlotsByStudioIDAndDate(id, date);
+
+        // Compare the current time with the date and time from the database
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        List<Slot> slotList = new ArrayList<>();
+        for (Slot slot : slots) {
+            LocalDate databaseDate = LocalDate.parse(slot.getDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            LocalTime databaseTime = LocalTime.parse(slot.getStartTime(), DateTimeFormatter.ofPattern("h:mm a"));
+            LocalDateTime databaseDateTime = LocalDateTime.of(databaseDate, databaseTime);
+
+            if (!currentDateTime.isAfter(databaseDateTime)) {
+                slotList.add(slot);
+            }
+        }
+
+        return ResponseUtils.get(slotList, HttpStatus.OK);
+    }
 
     public ResponseEntity<?> getSlotByID(String id) {
             if(slotRepository.findSlotBySlotID(id) == null) {
