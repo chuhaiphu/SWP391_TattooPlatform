53839ffc0355c8fdd9516eb6eea7598f89fb7802
chuhaiphu <phudvse171839@fpt.edu.vn>
2023-11-15 14:11:29 +0700

    Add view-lovers

--- src/main/java/SWP391/TattooPlatform/model/TattooLovers.java
+++ src/main/java/SWP391/TattooPlatform/model/TattooLovers.java
@@ -54,7 +54,4 @@ public class TattooLovers {
     @ToString.Exclude
     private UserStatus userStatus;
 
-
-
-
 }
--- src/main/resources/static/studiomanager/assets/js/view-lovers.js
+++ src/main/resources/static/studiomanager/assets/js/view-lovers.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     // Send an AJAX request to fetch artist data
     $.ajax({
         type: "GET",
-        url: "/tattoolovers/view-list", // Replace with the actual API endpoint
+        url: "/tattoolovers/all", // Replace with the actual API endpoint
         dataType: "json",
         success: function (data) {
-            for( var lover of data) {
+            for( var lover of data.content) {
                 renderloverData(lover);
             }
         },
@@ -15,19 +15,19 @@ $(document).ready(function () {
     });
 });
 
-function renderloverData(loverdata) {
-    var loverTable = $("#loverList_template").html();
-    loverTable = loverTable.replace("{{tattooLoveremail}}", loverdata.tattooLoveremail );
-    loverTable = loverTable.replace("{{fullname}}", loverdata.fullname );
-    loverTable = loverTable.replace("{{username}}", loverdata.username );
-    loverTable = loverTable.replace("{{password}}", loverdata.password );
-    loverTable = loverTable.replace("{{phonenumber}}", loverdata.phonenumber );
-    loverTable = loverTable.replace("{{address}}", loverdata.address );
-    loverTable = loverTable.replace("{{statusID}}", loverdata.statusID );
-
-
-
-    var added_lover = $("#lover");
-    $(loverTable).appendTo(added_lover);
-
+function renderloverData(lover) {
+    var added_lover = document.getElementById("lover");
+    console.log("30" + added_lover.innerHTML);
+    var loverUsername = lover.username;
+    added_lover.innerHTML =
+        added_lover.innerHTML +
+        `
+  <tr>
+  <td class="loverEmail" data-voucher-id="${loverUsername}">${lover.tattooLoveremail}</td>
+  <td class="loverName" data-voucher-id="${loverUsername}">${lover.fullname}</td>
+  <td class="loverUserName" data-voucher-id="${loverUsername}">${lover.username}</td>
+  <td class="phoneNumber" data-voucher-id="${loverUsername}">${lover.phonenumber}</td>
+  <td class="address" data-voucher-id="${loverUsername}">${lover.address}</td>
+</tr>
+`
 }
